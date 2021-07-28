8878887bce62b4b9ac47154ea629a2f0474f0a14
Xanthe McElroy <78177169+xanthemce@users.noreply.github.com>
2021-07-28 02:22:44 +0000

    changed the random to picking a random number from a list.

--- index.js
+++ index.js
@@ -1,25 +1,45 @@
+dice = [1, 2, 3, 4, 5, 6,]
+let length = dice.length
+
+roll1= Math.floor(Math.random() * length + 0)
+number1= (dice[roll1])
+
+
+
+
 let point2 = 0
 let point1 = 0
 let round = 0
 let lucky1 = 0
 let lucky2 = 0
 var playerRound = prompt('how many rounds would you like to play?')
-while((isNaN(playerRound))||(playerRound<1)||(playerRound == null)||(playerRound==' ')){
+while((isNaN(playerRound))||(playerRound<1)||(playerRound == null)||(playerRound==' ')||(playerRound % 1 != 0)){
   console.log('that is not a valid number. please enter a number')
   playerRound = prompt('how many rounds would you like to play?')
 }
 
 
 do{
-let dice1 = Math.floor(Math.random() * 6 + 1);
-let dice2 = Math.floor(Math.random() * 6 + 1);
+// let dice1 = Math.floor(Math.random() * 6 + 1);
+
 
   
 var play1 = prompt('player 1, would you like to roll the dice: yes or no:')
 if (play1 == 'yes'){
 console.log('here we go')
+console.log('you rolled a ' + number1)
 
-console.log('you rolled a ' + dice1)
+if(number1=)
+
+// if(dice1=2){
+//   console.log('you rolled a ' + dice1)
+//   point1++
+//   console.log('you get an extra point for rolling a two!')
+  
+// }
+// else{
+//   console.log('you rolled a ' + dice1)
+// }
 }
 else if(play1 == 'no'){
   console.log('okay thanks for playing')
@@ -30,6 +50,8 @@ else{
   play1 = prompt('would you like to roll the dice: yes or no:')
 }
 
+
+
 // 	if (dice1 = 2){
 //   console.log('you get an extra point for rolling a two')
 //   point1++
@@ -38,12 +60,13 @@ else{
 // lucky1++
 // }
   
-
+// let dice2 = Math.floor(Math.random() * 6 + 1);
 
 var play2 = prompt('player 2, would you like to roll the dice: yes or no:')
 if (play2 == 'yes'){
 console.log('here we go')
-console.log('you rolled a ' + dice2)
+
+
 }
 else if(play2 == 'no'){
   console.log('okay thanks for playing')
@@ -90,12 +113,12 @@ else if(point1>point2){
 else{
   console.log('it was a tie!')
 }
-if(lucky1<lucky2){
-  console.log('player 2 got more sixes. They got ' + lucky2 + ' sixes')
-}
-else if(lucky1>lucky2){
-  console.log('player 1 got more sixes. They got ' + lucky1 + ' sixes')
-}
-else{
-  console.log('you got the same amount of sixes')
-}
+// if(lucky1<lucky2){
+//   console.log('player 2 got more sixes. They got ' + lucky2 + ' sixes')
+// }
+// else if(lucky1>lucky2){
+//   console.log('player 1 got more sixes. They got ' + lucky1 + ' sixes')
+// }
+// else{
+//   console.log('you got the same amount of sixes')
+// }
